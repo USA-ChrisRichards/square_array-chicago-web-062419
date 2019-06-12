c8a8f6ddb6db71e12dab7391f7f4d44aa6275b7b
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-12 19:52:43 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,11 +1,11 @@
-#def square_array(array)
-  #new_array = []
-  #array.each { |num|  new_array << num ** 2 }
-  #new_array
-#end
-
 def square_array(array)
   new_array = []
-  puts array.collect { |num| num ** 2 }
+  array.each { |num|  new_array << num ** 2 }
+  new_array
 end
+
+#def square_array(array)
+ # new_array = []
+ # puts array.collect { |num| num ** 2 }
+#end
 
